ba96661363a4f69bda58ad36707abc1cb5f35604
wenhao <wenhaogoh1998@gmail.com>
2022-03-07 19:16:39 +0800

    chore: create repository

---
+++ live-to-tilt/Main.swift
@@ -0,0 +1,17 @@
+//
+//  live_to_tiltApp.swift
+//  live-to-tilt
+//
+//  Created by Wen Hao on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Main: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
