ec05fbdbb9cffcb9934c0155beb723f150e3a7b7
wenhao <wenhaogoh1998@gmail.com>
2022-03-10 23:15:17 +0800

    refactor: abstract layer and image attributes

--- live-to-tilt/GameEngine/Components/RenderableComponent.swift
+++ live-to-tilt/GameEngine/Components/RenderableComponent.swift
@@ -1,24 +1,27 @@
 import CoreGraphics
 
 class RenderableComponent: Component {
-    var imageName: String
+    var image: ImageAsset
 
     var position: CGPoint
     var size: CGSize
     var rotation: CGFloat
 
     var opacity: Double
+    var layer: Layer
 
-    init(imageName: String,
+    init(image: ImageAsset,
          position: CGPoint,
          size: CGSize,
          rotation: CGFloat = 0.0,
-         opacity: Double = 1.0) {
-        self.imageName = imageName
+         opacity: Double = 1.0,
+         layer: Layer = .everything) {
+        self.image = image
         self.position = position
         self.size = size
         self.rotation = rotation
         self.opacity = opacity
+        self.layer = layer
     }
 }
 
---
+++ live-to-tilt/Utilities/Layer.swift
@@ -0,0 +1,5 @@
+enum Layer: Double {
+    case enemy = 0.2
+    case player = 0.1
+    case everything = 0
+}
