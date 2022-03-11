3ae397cc76b04bf0d51b8e7e99bc2204abeb612d
Maxx Chan <maxxyh@gmail.com>
2022-03-12 02:15:54 +0800

    chore: merging main changes

--- live-to-tilt/Utilities/Constants.swift
+++ live-to-tilt/Utilities/Constants.swift
@@ -4,6 +4,12 @@ struct Constants {
     // Rendering
     static let framesPerSecond: Int = 60
 
+    // Audio
+    static let audioFadeDuration: Double = 1
+    static let defaultSoundtrackVolume: Float = 1
+    static let minSoundtrackVolume: Float = 0
+    static let maxSoundtrackVolume: Float = 2
+
     // physicsEngine Constants
     static let physicsBodyMinimumSize: CGFloat = 0.05
     static let restitution: CGFloat = 0.8
