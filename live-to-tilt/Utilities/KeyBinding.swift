9d08e42044255cd3a5657ee268c4c888a8e7e8fb
Chester How <chesterhow@gmail.com>
2022-03-15 23:50:50 +0800

    chore: cleanup

--- live-to-tilt/Utilities/KeyBinding.swift
+++ live-to-tilt/Utilities/KeyBinding.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct KeyBinding: Equatable {
+struct KeyBinding {
     static let up: KeyEquivalent = "w"
     static let down: KeyEquivalent = "s"
     static let left: KeyEquivalent = "a"
