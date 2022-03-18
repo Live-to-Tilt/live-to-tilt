bb4e8e4231bdbfea135f78b4f0101c92ec43c4cb
Kai Siang <kslui@live.com.sg>
2022-03-18 15:02:05 +0800

    fix: move physics management out of GameEngine into PhysicsSystem

--- live-to-tilt/GameEngine/GameEngine.swift
+++ live-to-tilt/GameEngine/GameEngine.swift
@@ -6,9 +6,6 @@ class GameEngine {
     let nexus = Nexus()
     let systems: [System]
 
-    // Physics Engine
-    let physicsWorld = PhysicsWorld()
-
     let renderableSubject = PassthroughSubject<[RenderableComponent], Never>()
     var renderablePublisher: AnyPublisher<[RenderableComponent], Never> {
         renderableSubject.eraseToAnyPublisher()
@@ -24,28 +21,20 @@ class GameEngine {
         ]
 
         setUpEntities()
-
-        setUpPhysicsEngine()
     }
 
     func update(deltaTime: CGFloat, inputForce: CGVector) {
-        updatePhysicsBodies(deltaTime: deltaTime)
-        updatePlayer(inputForce: inputForce)
         updateSystems(deltaTime: deltaTime)
+        updatePlayer(inputForce: inputForce)
         publishRenderables()
     }
 
     private func setUpEntities() {
         nexus.createPlayer()
     }
-
-    private func setUpPhysicsEngine() {
-        physicsWorld.contactDelegate = self
-    }
-
-    private func updatePhysicsBodies(deltaTime: CGFloat) {
-        let physicsBodies = nexus.getComponents(of: PhysicsComponent.self).map { $0.physicsBody }
-        physicsWorld.update(physicsBodies, deltaTime: deltaTime)
+    
+    private func updateSystems(deltaTime: CGFloat) {
+        systems.forEach { $0.update(deltaTime: deltaTime) }
     }
 
     private func updatePlayer(inputForce: CGVector) {
@@ -54,62 +43,8 @@ class GameEngine {
         }
         playerComponent.inputForce = inputForce
     }
-
-    private func updateSystems(deltaTime: CGFloat) {
-        systems.forEach { $0.update(deltaTime: deltaTime) }
-    }
-
+    
     private func publishRenderables() {
         renderableSubject.send(nexus.getComponents(of: RenderableComponent.self))
-    }
-}
-
-// MARK: PhysicsCollisionDelegate
-// `GameEngine` is a `PhysicsCollisionDelegate` since it implements custom game logic when a collision is detected
-// in the Physics Engine.
-extension GameEngine: PhysicsCollisionDelegate {
-    func didBegin(_ collision: Collision) {
-        guard let entityA = getEntity(from: collision.bodyA),
-                let entityB = getEntity(from: collision.bodyB) else {
-            return
-        }
-
-        if isCollisionBetweenPlayerAndPowerup(entityA: entityA, entityB: entityB) {
-            respondToCollisionBetweenPlayerAndPowerup(entityA: entityA, entityB: entityB)
-        }
-    }
-
-    func didEnd(_ collision: Collision) {
-        }
-
-    private func getEntity(from physicsBody: PhysicsBody) -> Entity? {
-        let physicsComponents = nexus.getComponents(of: PhysicsComponent.self)
-
-        guard let physicsComponent = physicsComponents.first(where: { $0.physicsBody === physicsBody }) else {
-            return nil
-        }
-
-        return physicsComponent.entity
-    }
-
-    private func isCollisionBetweenPlayerAndPowerup(entityA: Entity, entityB: Entity) -> Bool {
-        let isPowerupPlayerCollision = nexus.hasComponent(PowerupComponent.self, in: entityA)
-            && nexus.hasComponent(PlayerComponent.self, in: entityB)
-
-        let isPlayerPowerupCollision = nexus.hasComponent(PlayerComponent.self, in: entityA)
-            && nexus.hasComponent(PowerupComponent.self, in: entityB)
-
-        return isPowerupPlayerCollision || isPlayerPowerupCollision
-    }
-
-    private func respondToCollisionBetweenPlayerAndPowerup(entityA: Entity, entityB: Entity) {
-        let entityWithPowerupComponent = nexus.hasComponent(PowerupComponent.self, in: entityA) ? entityA : entityB
-
-        guard let powerupComponent = nexus.getComponent(of: PowerupComponent.self,
-                                                        for: entityWithPowerupComponent) else {
-            return
-        }
-
-        powerupComponent.activate()
     }
 }
--- live-to-tilt/GameEngine/Systems/PhysicsSystem.swift
+++ live-to-tilt/GameEngine/Systems/PhysicsSystem.swift
@@ -2,11 +2,29 @@ import CoreGraphics
 
 class PhysicsSystem: System {
     let nexus: Nexus
+    
+    let physicsWorld = PhysicsWorld()
 
     init(nexus: Nexus) {
         self.nexus = nexus
+        self.physicsWorld.contactDelegate = self
     }
+    
+    func update(deltaTime: CGFloat) {
+        let entities = nexus.getEntities(with: PhysicsComponent.self)
 
+        entities.forEach { entity in
+            updateRenderable(entity)
+        }
+        
+        updatePhysicsBodies(deltaTime: deltaTime)
+    }
+    
+    private func updatePhysicsBodies(deltaTime: CGFloat) {
+        let physicsBodies = nexus.getComponents(of: PhysicsComponent.self).map { $0.physicsBody }
+        physicsWorld.update(physicsBodies, deltaTime: deltaTime)
+    }
+    
     private func updateRenderable(_ entity: Entity) {
         guard let physicsComponent = nexus.getComponent(of: PhysicsComponent.self, for: entity) else {
             return
@@ -19,12 +37,54 @@ class PhysicsSystem: System {
             renderableComponent.rotation = physicsComponent.physicsBody.rotation
         }
     }
+}
 
-    func update(deltaTime: CGFloat) {
-        let entities = nexus.getEntities(with: PhysicsComponent.self)
-
-        entities.forEach { entity in
-            updateRenderable(entity)
+// MARK: PhysicsCollisionDelegate
+// `PhysicsSystem` is a `PhysicsCollisionDelegate` since it implements custom game logic when a collision is detected
+// in the Physics Engine.
+extension PhysicsSystem: PhysicsCollisionDelegate {
+    func didBegin(_ collision: Collision) {
+        guard let entityA = getEntity(from: collision.bodyA),
+                let entityB = getEntity(from: collision.bodyB) else {
+            return
         }
+
+        if isCollisionBetweenPlayerAndPowerup(entityA: entityA, entityB: entityB) {
+            respondToCollisionBetweenPlayerAndPowerup(entityA: entityA, entityB: entityB)
+        }
+    }
+
+    func didEnd(_ collision: Collision) {
+        }
+
+    private func getEntity(from physicsBody: PhysicsBody) -> Entity? {
+        let physicsComponents = nexus.getComponents(of: PhysicsComponent.self)
+
+        guard let physicsComponent = physicsComponents.first(where: { $0.physicsBody === physicsBody }) else {
+            return nil
+        }
+
+        return physicsComponent.entity
+    }
+
+    private func isCollisionBetweenPlayerAndPowerup(entityA: Entity, entityB: Entity) -> Bool {
+        let isPowerupPlayerCollision = nexus.hasComponent(PowerupComponent.self, in: entityA)
+            && nexus.hasComponent(PlayerComponent.self, in: entityB)
+
+        let isPlayerPowerupCollision = nexus.hasComponent(PlayerComponent.self, in: entityA)
+            && nexus.hasComponent(PowerupComponent.self, in: entityB)
+
+        return isPowerupPlayerCollision || isPlayerPowerupCollision
+    }
+
+    private func respondToCollisionBetweenPlayerAndPowerup(entityA: Entity, entityB: Entity) {
+        let entityWithPowerupComponent = nexus.hasComponent(PowerupComponent.self, in: entityA) ? entityA : entityB
+
+        guard let powerupComponent = nexus.getComponent(of: PowerupComponent.self,
+                                                        for: entityWithPowerupComponent) else {
+            return
+        }
+
+        powerupComponent.activate()
     }
 }
