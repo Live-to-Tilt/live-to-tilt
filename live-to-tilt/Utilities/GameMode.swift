386596cd761fb92411289a8480bef815aafb980c
Chester How <chesterhow@gmail.com>
2022-04-02 17:22:45 +0800

    style: gamemode selection screen (#44)
    * style: gamemode selection screen
    
    * chore: update copy

--- live-to-tilt/Utilities/GameMode.swift
+++ live-to-tilt/Utilities/GameMode.swift
@@ -1,6 +1,32 @@
 enum GameMode: String, CaseIterable, Identifiable {
-    case survival
-    case gauntlet
+    case survival = "Survival"
+    case gauntlet = "Gauntlet"
 
     var id: Self { self }
+}
+
+extension GameMode {
+    var emoji: String {
+        switch self {
+        case .survival:
+            return "☠️"
+        case .gauntlet:
+            return "⚔️"
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .survival:
+            return """
+                Use flashy powerups to defeat endless waves of enemies. \
+                How long can you survive?
+                """
+        case .gauntlet:
+            return """
+                Think Flappy Bird, but Live to Tilt. Put your fine control skills \
+                to the test as you weave through complex enemy formations!
+                """
+        }
+    }
 }
