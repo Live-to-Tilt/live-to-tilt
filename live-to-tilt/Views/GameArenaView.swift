1eaba5bc27cb7eca68a2b69b0824d2c544327bbd
Chester How <chesterhow@gmail.com>
2022-04-04 19:26:43 +0800

    chore: cleanup

--- live-to-tilt/Views/GameArenaView.swift
+++ live-to-tilt/Views/GameArenaView.swift
@@ -60,10 +60,7 @@ struct GameArenaView: View {
     }
 
     private func Score() -> some View {
-//        let score = viewModel.gameStateComponent?.score ?? 0
-        let score = viewModel.gameEngine.gameStats.score
-
-        return Text("\(score)")
+        Text("\(viewModel.gameEngine.gameStats.score)")
             .font(.system(size: 200, weight: .heavy))
             .monospacedDigit()
             .opacity(0.15)
