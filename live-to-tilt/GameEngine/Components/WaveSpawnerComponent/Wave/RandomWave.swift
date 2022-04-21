e60b21c54e065fdc9f20379921541b877a4c77d2
Wen Hao <52305097+wenhaogoh@users.noreply.github.com>
2022-04-21 22:59:15 +0800

    fix: fix homing for multiplayer (#91)

--- live-to-tilt/GameEngine/Components/WaveSpawnerComponent/Wave/RandomWave.swift
+++ live-to-tilt/GameEngine/Components/WaveSpawnerComponent/Wave/RandomWave.swift
@@ -3,27 +3,59 @@ import CoreGraphics
 
 class RandomWave: Wave {
     func coroutine(nexus: Nexus) {
-        guard let playerEntity = nexus.getEntity(with: PlayerComponent.self) else {
-            return
-        }
-
         let startTime: DispatchTime = .now()
         var delay: Double = .zero
         for _ in 0..<Constants.randomWaveEnemyCount {
             DispatchQueue.main.asyncAfter(deadline: startTime + delay) {
-                self.spawnEnemy(nexus: nexus, target: playerEntity)
+                self.spawnEnemy(nexus: nexus)
             }
             delay += Constants.randomWaveDelay
         }
     }
 
-    private func spawnEnemy(nexus: Nexus, target playerEntity: Entity) {
+    private func spawnEnemy(nexus: Nexus) {
         let spawnPosition = GameUtils.generateRandomSpawnPosition(forEntityOfWidth: Constants.enemyDiameter,
                                                                   height: Constants.enemyDiameter)
+        guard let playerEntity = getNearestPlayerEntity(from: spawnPosition, nexus: nexus) else {
+            return
+        }
         let movementA: Movement = BaseMovement()
         var movementB: Movement = BaseMovement()
         movementB = HomingMovementDecorator(movement: movementB, target: playerEntity)
         let movementAB = ConnectedMovement(movementA, for: Constants.enemySpawnDelay, then: movementB)
         nexus.createEnemy(position: spawnPosition, movement: movementAB)
+    }
+
+    private func getNearestPlayerEntity(from position: CGPoint, nexus: Nexus) -> Entity? {
+        let playerComponents = nexus.getComponents(of: PlayerComponent.self)
+        guard
+            let currNearestPlayerComponent = playerComponents.first,
+            let currNearestPlayerPosition = getPosition(of: currNearestPlayerComponent.entity, nexus: nexus) else {
+                return nil
+        }
+
+        var currNearestPlayerEntity = currNearestPlayerComponent.entity
+        var currMinimumDistance = (position - currNearestPlayerPosition).magnitudeSquared
+
+        for playerComponent in playerComponents {
+            let playerEntity = playerComponent.entity
+            guard let playerPosition = getPosition(of: playerEntity, nexus: nexus) else {
+                continue
+            }
+            let distance = (position - playerPosition).magnitudeSquared
+            if distance < currMinimumDistance {
+                currMinimumDistance = distance
+                currNearestPlayerEntity = playerEntity
+            }
+        }
+
+        return currNearestPlayerEntity
+    }
+
+    private func getPosition(of entity: Entity, nexus: Nexus) -> CGPoint? {
+        let physicsComponent = nexus.getComponent(of: PhysicsComponent.self, for: entity)
+        let phyiscsBody = physicsComponent?.physicsBody
+        let position = phyiscsBody?.position
+        return position
     }
 }
